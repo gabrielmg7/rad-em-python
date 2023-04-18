90baef05a283a1939ccb4bce19bb24309d71ca5a
gabrielmg7 <201902661559@alunos.estacio.br>
2023-04-18 20:25:06 -0300

    conexao simples com bd

--- exercicios/exercicio-sava/db.py
+++ exercicios/exercicio-sava/db.py
@@ -1,19 +1,17 @@
-import sqlite3
+import sqlite3 as connector
 
-# Conecta-se ao banco de dados ou cria um novo se ele não existir
-connection = sqlite3.connect("mydatabase.db")
+connection = connector.connect("mydatabase.db")  # Conecta-se ao banco de dados ou cria um novo se ele não existir
+cursor = connection.cursor()  # Cria conexão do Cursor
 
-# Cria a tabela "students" com as colunas "name" e "grade"
-cursor = connection.cursor()
-cursor.execute("CREATE TABLE students (name TEXT, grade INTEGER)")
+cursor.execute(  # Cria a tabela "students" com as colunas "nome" e "grade"
+    "CREATE TABLE students (nome TEXT, grade INTEGER)"
+)
 
 # Insere alguns dados na tabela
-cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ("Alice", 95))
-cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ("Bob", 84))
-cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", ("Charlie", 72))
+cursor.execute("INSERT INTO students (nome, grade) VALUES (?, ?)", ("Alice", 95))
+cursor.execute("INSERT INTO students (nome, grade) VALUES (?, ?)", ("Bob", 84))
+cursor.execute("INSERT INTO students (nome, grade) VALUES (?, ?)", ("Charlie", 72))
 
-# Salva as alterações no banco de dados
-connection.commit()
-
-# Fecha a conexão com o banco de dados
-connection.close()
+connection.commit()  # Salva as alterações no banco de dados
+connection.close()  # Encerra a conexão com o banco de dados
+cursor.close()  # Encerra conexão do Cursor
